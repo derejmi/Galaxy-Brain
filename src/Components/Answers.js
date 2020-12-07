b594087459c9d15a05f94249252aced1a6504a15
derejmi <derejemireyigbe@gmail.com>
2020-12-07 22:59:29 +0000

    fix weird bug where 1st question of quiz  always repeated an answer option

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -4,13 +4,15 @@ const Answers = (props) => {
   const arrayIndex = [props.questionNumber] - 1;
   const correct = props.data[arrayIndex]["correct_answer"];
   const answersArray = props.data[arrayIndex]["incorrect_answers"];
-  const randomNo = Math.floor(Math.random() * answersArray.length + 1);
+  const randomNo = Math.floor(Math.random() * answersArray.length);
   answersArray.splice(randomNo, 0, correct);
 
   return (
     <div>
-      {answersArray.map((elem, idx) => (
-        <button key={idx}>{elem}</button>
+      {answersArray.slice(0, 4).map((elem, idx) => (
+        <button onClick={props.handleAnswerClick} key={idx + 1}>
+          {elem}
+        </button>
       ))}
     </div>
   );
