51c7128bf69cb2ed6e514aa04110a2ddad888524
derejmi <derejemireyigbe@gmail.com>
2020-12-07 18:03:29 +0000

    add greeting quiz data logic

--- src/Containers/QuestionContainer.js
+++ src/Containers/QuestionContainer.js
@@ -1,57 +1,73 @@
 import React from "react";
 // import Score from "../components/Score.js";
-// import Question from "../components/Question.js";
-// import Answers from "../components/Answers.js";
-import Form from "../components/Form/index"
+import Question from "../components/Question.js";
+import Answers from "../components/Answers.js";
+import Form from "../components/Form/index";
 
 class QuestionContainer extends React.Component {
   //URL not needed just for reference
   state = {
-      questionNumber: 1,
-      difficulty: "",
-      players: "",
-      rounds: "",
-      selection: []
-  }
+    questionNumber: 0,
+    difficulty: "",
+    players: "",
+    rounds: "",
+    selection: [],
+  };
   handleInputChange = (e) => {
     e.preventDefault();
     this.setState({
-        [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
-}
+  };
 
-
-handleClick = (event) => {
+  handleClick = (event) => {
     event.preventDefault();
-    this.fetchAPI()
-}
+    this.fetchAPI();
+  };
 
-fetchAPI = () => {
-  console.log("Lets fetch some json")
-  const r = this.state.rounds
-  const p = this.state.players
-  const multi = p*r
-  let url  = `https://opentdb.com/api.php?amount=${multi}&category=10&difficulty=${this.state.difficulty}&type=multiple`;
-  fetch(url)
-      .then(res => res.json())
-      .then(data => {
-          console.log(data);
-          this.setState({selection: data.results })
-          console.log("check here")
-          console.log(this.state.selection)
-      })
-}
-
-  
+  fetchAPI = () => {
+    console.log("Lets fetch some json");
+    const r = this.state.rounds;
+    const p = this.state.players;
+    const multi = p * r;
+    let url = `https://opentdb.com/api.php?amount=${multi}&category=10&difficulty=${this.state.difficulty}&type=multiple`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({ selection: data.results, questionNumber: 1 });
+        console.log("check here");
+        console.log(this.state.selection);
+      });
+  };
 
   render() {
     // console.log(this.state.questionData);
     return (
-
-      
       <div>
-        <Form handleClick={this.handleClick} handleInputChange={this.handleInputChange} rounds={this.state.rounds} players={this.state.players} difficulty={this.state.difficulty}/>
-
+        <div>
+          <Form
+            handleClick={this.handleClick}
+            handleInputChange={this.handleInputChange}
+            rounds={this.state.rounds}
+            players={this.state.players}
+            difficulty={this.state.difficulty}
+          />
+        </div>
+        {this.state.questionNumber ? (
+          <div>
+            <Question
+              data={this.state.selection}
+              questionNumber={this.state.questionNumber}
+            />
+            <Answers
+              data={this.state.selection}
+              questionNumber={this.state.questionNumber}
+            />
+          </div>
+        ) : (
+          <h1>Welcome to Galaxy Brain </h1>
+        )}
       </div>
     );
   }
