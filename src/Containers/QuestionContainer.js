a6532a3336313f173420a78cda5801990d383b8c
derejmi <derejemireyigbe@gmail.com>
2020-12-07 16:13:13 +0000

    add container for form, questions, answers and scores

--- src/Containers/QuestionContainer.js
+++ src/Containers/QuestionContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import data from "../api/data.js";
+import data from "../api/dummyData.js";
 import Score from "../Components/Score.js";
 import Question from "../Components/Question.js";
 import Answers from "../Components/Answers.js";
@@ -10,18 +10,23 @@ class QuestionContainer extends React.Component {
     URL:
       "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple",
     questionData: data,
-    questionNumber: 1,
+    questionNumber: 9,
   };
 
   render() {
+    console.log(this.state.questionData);
     return (
       <div>
+        <Form />
         <Score />
         <Question
           data={this.state.questionData.results}
           questionNumber={this.state.questionNumber}
         />
-        <Answers data={this.state.questionData.results} />
+        <Answers
+          data={this.state.questionData.results}
+          questionNumber={this.state.questionNumber}
+        />
       </div>
     );
   }
