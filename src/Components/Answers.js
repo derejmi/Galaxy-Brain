bd93bb9394079e044044fd0e120898dc3fa94fac
derejmi <derejemireyigbe@gmail.com>
2020-12-08 10:33:45 +0000

    correct answer logic

--- src/Components/Answers.js
+++ src/Components/Answers.js
@@ -1,21 +1,42 @@
 import React from "react";
 
-const Answers = (props) => {
-  const arrayIndex = [props.questionNumber] - 1;
-  const correct = props.data[arrayIndex]["correct_answer"];
-  const answersArray = props.data[arrayIndex]["incorrect_answers"];
-  const randomNo = Math.floor(Math.random() * answersArray.length);
-  answersArray.splice(randomNo, 0, correct);
+// const Answers = (props) => {
+//   debugger;
+//   const arrayIndex = [props.questionNumber] - 1;
+//   const correct = props.data[arrayIndex]["correct_answer"];
+//   const answersArray = props.data[arrayIndex]["incorrect_answers"];
+//   const randomNo = Math.floor(Math.random() * answersArray.length);
+//   answersArray.splice(randomNo, 0, correct);
 
-  return (
-    <div>
-      {answersArray.slice(0, 4).map((elem, idx) => (
-        <button onClick={props.handleAnswerClick} key={idx + 1}>
-          {elem}
-        </button>
-      ))}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {answersArray.map((elem, idx) => (
+//         <button onClick={props.handleAnswerClick} key={idx + 1}>
+//           {elem}
+//         </button>
+//       ))}
+//     </div>
+//   );
+// };
+
+class Answers extends React.Component {
+  render() {
+    const arrayIndex = [this.props.questionNumber] - 1;
+    const correct = this.props.data[arrayIndex]["correct_answer"];
+    const answersArray = this.props.data[arrayIndex]["incorrect_answers"];
+    const randomNo = Math.floor(Math.random() * answersArray.length);
+    const newArray = [...answersArray];
+    newArray.splice(randomNo, 0, correct);
+    return (
+      <div>
+        {newArray.map((elem, idx) => (
+          <button onClick={this.props.handleAnswerClick} key={idx + 1}>
+            {elem}
+          </button>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Answers;
