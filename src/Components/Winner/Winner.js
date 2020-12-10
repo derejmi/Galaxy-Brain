627c677b9c36c450a1ea4b90093314dd52333c09
Tempestx4 <mavindidzetonderai@yahoo.com>
2020-12-10 11:24:16 +0000

    modified player array and removed unsed code in winner.js

--- src/Components/Winner/Winner.js
+++ src/Components/Winner/Winner.js
@@ -3,80 +3,54 @@ import React from "react";
 const Winner = (props) => {
     const playerNumber = props.players
     const playerArr = props.playerData
-    const arr=[]
-
-    const pickHighest = array => {
-        const res = {
-          player: '',
-          score: -Infinity
-        };
-        array.forEach(element => {
-          const { player, score } = element;
-          if(score >= res.score){
-            res.player = player;
-            res.score = score;
-            element = {}
-            element.player = res.player
-            element.score = res.score
-            arr.push(element)
-          }
-        }); 
-        return res;
-      };
-     pickHighest(playerArr)
-
-
-
-      const finalArr = arr.slice(0,playerNumber)
-
-
-
-const winner = (arr) =>{
+    const finalArr = playerArr.slice(0,playerNumber)
+    console.log("playerArr", playerArr)
+    const winner = (playerArr) =>{
 
     const winnerArr = []
     let highScore = 0
 
-    for(let i = 0 ; i < arr.length; i++){
-        let currentElem = arr[i]
-        if(currentElem.score > highScore){
-           highScore = currentElem.score
-        }
+    for(let i = 0 ; i < playerArr.length; i++){
+      let currentElem = playerArr[i]
+      if(currentElem.score > highScore){
+        highScore = currentElem.score
+      }
     }
 
-    for(let i = 0; i < arr.length; i++){
-        let currentElem = arr[i]
-        if(currentElem.score === highScore){
-            winnerArr.push(currentElem.player+' | Score:'+highScore)
-        }
+    for(let i = 0; i < playerArr.length; i++){
+      let currentElem = playerArr[i]
+      if(currentElem.score === highScore){
+        winnerArr.push(currentElem.player+' | Score:'+highScore)
+      }
     }
-
     return winnerArr
-
-} 
+  } 
 
 const refreshPage = ()=>{
-    window.location.reload();  }
+    window.location.reload();
+  }
 
 const winningPlayer = winner(finalArr)
 
-    const str = winningPlayer.join(", ")
+const str = winningPlayer.join(", ")
 
-    console.log("winners",winningPlayer)
-    if(winningPlayer.length > 1){
-      return (
-        <div>
-          <h1>Game Over! Winners were ...{str}</h1>
-          <button onClick={refreshPage}>Click to reload!</button>
-        </div>
-      );
-    }else{
-      return (
-        <div>
-          <h1>Game Over! Winner was ...{str}</h1>
-          <button onClick={refreshPage}>Click to reload!</button>
-        </div>
+console.log("winners",winningPlayer)
+
+if(winningPlayer.length > 1){
+  return (
+  <div>
+    <h1>Game Over! Winners were ...{str}</h1>
+    <button onClick={refreshPage}>Click to reload!</button>
+    </div>
+    );
+  }else{
+    return (
+    <div>
+      <h1>Game Over! Winner was ...{str}</h1>
+      <button onClick={refreshPage}>Click to reload!</button>
+      </div>
       );
     }
-};
+  };
 
 export default Winner;
